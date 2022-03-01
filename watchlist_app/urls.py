e88ea668640220c0c5b4c92b006acc1d7bafd100
= <=>
2022-03-01 21:36:38 +0600

    get Individual Elements view

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.movie_list, name="movie-list"),
     path('list/', views.movie_list, name="movie-list"),
+    path('<int:pk>/', views.movie_details, name="movie-details"),
 ]

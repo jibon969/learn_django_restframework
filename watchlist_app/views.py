8b5b7d63b516ddc75e699ce264d0fd02328a9f37
= <=>
2022-03-01 21:26:06 +0600

    added model Movie & added movie_list view

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Movie
 
-# Create your views here.
+
+def movie_list(request):
+    movie = Movie.objects.all()
+    data = {
+        'movies': list(movie.values())
+    }
+    return JsonResponse(data)
+

f3d3e5a40da2c02a6ded05a6749c392512488d40
= <=>
2022-03-01 21:48:14 +0600

    added install djangorestframework

--- build_REST_APIs_with_Django/settings.py
+++ build_REST_APIs_with_Django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     # Custom app
     'watchlist_app.apps.WatchlistAppConfig',
     'django.contrib.admin',

e7e27b507d8fbeb8a9d61447f4d938147b9315d4
= <=>
2022-03-12 15:25:37 +0600

    added nested serializer

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -18,13 +18,13 @@ urlpatterns = [
     path('watch/', WatchListAv.as_view(), name="watch-list"),
     path('<int:pk>/', WatchListDetailAv.as_view(), name="watch-detail"),
 
-    
+
     path('stream/', stream_plat_form_list, name="stream-list"),
     path('<int:pk>/', stream_plat_form_details, name="stream-details"),
 
     # Api view
     path('stream/', StreamPlatFormAv.as_view(), name="stream-list"),
-    path('<int:pk>/', StreamPlatFormDetailAv.as_view(), name="stream-detail"),
+    path('stream/<int:pk>/', StreamPlatFormDetailAv.as_view(), name="stream-detail"),
 
 
 ]
